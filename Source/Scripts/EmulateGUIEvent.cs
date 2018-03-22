b7c725641f93b8ec2e2bf37d3c94d7bb9c843c3b
PanYoucheng <aq_1000@sina.com>
2018-03-22 17:44:07 +0800

    Add files via upload

---
+++ Source/Scripts/EmulateGUIEvent.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FairyGUI
+{
+    public enum EmulateGUIEventType
+    {
+        KeyDown,
+        KeyUp,
+        scrollWheel
+    }
+
+    public struct EmulateGUIEvent
+    {
+        public EmulateGUIEventType type;
+        public KeyCode keyCode;
+        public EventModifiers modifiers;
+        public char character;
+        public Vector2 delta;
+
+        public EmulateGUIEvent(EmulateGUIEventType t, KeyCode kc, EventModifiers em, char c, Vector2 dlt)
+        {
+            type = t;
+            keyCode = kc;
+            modifiers = em;
+            character = c;
+            delta = dlt;
+        }
+    }
+}
