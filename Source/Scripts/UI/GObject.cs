bc43e5036cf92da45b8eb975692e0a2f189bb5fb
guzhu@fairygui.com <guzhu@fairygui.com>
2018-10-25 11:04:54 +0800

    fixed issue in GObject.Dispose

--- Source/Scripts/UI/GObject.cs
+++ Source/Scripts/UI/GObject.cs
@@ -1538,8 +1538,22 @@ namespace FairyGUI
 			return this.displayObject.TransformRect(rect, targetSpace.displayObject);
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
+		public bool isDisposed
+		{
+			get { return _disposed; }
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
 		virtual public void Dispose()
 		{
+			if (_disposed)
+				return;
+
 			_disposed = true;
 
 			RemoveFromParent();
@@ -1554,76 +1568,121 @@ namespace FairyGUI
 			data = null;
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GImage asImage
 		{
 			get { return this as GImage; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GComponent asCom
 		{
 			get { return this as GComponent; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GButton asButton
 		{
 			get { return this as GButton; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GLabel asLabel
 		{
 			get { return this as GLabel; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GProgressBar asProgress
 		{
 			get { return this as GProgressBar; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GSlider asSlider
 		{
 			get { return this as GSlider; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GComboBox asComboBox
 		{
 			get { return this as GComboBox; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GTextField asTextField
 		{
 			get { return this as GTextField; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GRichTextField asRichTextField
 		{
 			get { return this as GRichTextField; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GTextInput asTextInput
 		{
 			get { return this as GTextInput; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GLoader asLoader
 		{
 			get { return this as GLoader; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GList asList
 		{
 			get { return this as GList; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GGraph asGraph
 		{
 			get { return this as GGraph; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GGroup asGroup
 		{
 			get { return this as GGroup; }
 		}
 
+		/// <summary>
+		/// 
+		/// </summary>
 		public GMovieClip asMovieClip
 		{
 			get { return this as GMovieClip; }
