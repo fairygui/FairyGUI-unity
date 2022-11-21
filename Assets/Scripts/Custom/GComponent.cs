c862c1433d8ccc5199ea0a9c035332a7fe949c28
yhchen <myself559@gmail.com>
2022-11-21 18:24:28 +0800

    feat: GComponent 增加RemoteChildren

---
+++ Assets/Scripts/Custom/GComponent.cs
@@ -0,0 +1,21 @@
+﻿// ***************纵梦互娱***************
+// 文件名:   GComponent.cs
+// 创建作者: yhchen
+// 创建时间: 2022/10/21/23:34
+// 功能描述:
+// 重要更新:
+// ***************纵梦互娱***************
+
+namespace FairyGUI
+{
+    public partial class GComponent
+    {
+        /// <summary>
+        /// Remove all children.
+        /// </summary>
+        public void RemoveChildren(bool dispose)
+        {
+            RemoveChildren(0, -1, dispose);
+        }
+    }
+}
