077e3e0ab74627f165fa3281ba8fe03f958948c2
chiuanwei <weizhuquan@vip.qq.com>
2023-02-15 18:59:03 +0800

    fix 修复发布错误editor类,兼容2021版本

--- Assets/Plugins/FairyGUI/Editor/UIPanelEditor.cs
+++ Assets/Plugins/FairyGUI/Editor/UIPanelEditor.cs
@@ -144,7 +144,7 @@ namespace FairyGUIEditor
             Vector3 pos = panel.GetUIWorldPosition();
             float sizeFactor = HandleUtility.GetHandleSize(pos);
 #if UNITY_2017_1_OR_NEWER
-            var fmh_147_58_638029390776941523 = Quaternion.identity; Vector3 newPos = Handles.FreeMoveHandle(pos, sizeFactor, Vector3.one, Handles.ArrowHandleCap);
+            var fmh_147_58_638029390776941523 = Quaternion.identity; Vector3 newPos = Handles.FreeMoveHandle(pos,Quaternion.identity,  sizeFactor, Vector3.one, Handles.ArrowHandleCap);
 #else
             Vector3 newPos = Handles.FreeMoveHandle(pos, Quaternion.identity, sizeFactor, Vector3.one, Handles.ArrowCap);
 #endif
